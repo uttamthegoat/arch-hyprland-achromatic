9d988c050a9be155d90c582a08dc827008726e7e
Uttam Prabhu <19516uttam@gmail.com>
2026-04-24 01:28:01 +0530

    [updated]: setup neovim with lazy

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,35 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
+-- core configs
+require 'core.keymaps'
+require 'core.options'
+require 'core.snippets'
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+  local out = vim.fn.system { 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath }
+  if vim.v.shell_error ~= 0 then error('Error cloning lazy.nvim:\n' .. out) end
+end
+
+---@type vim.Option
+local rtp = vim.opt.rtp
+rtp:prepend(lazypath)
+
+
+require('lazy').setup({
+    require 'plugins.colortheme',
+    require 'plugins.neotree',
+    require 'plugins.bufferline',
+    require 'plugins.lualine',
+    require 'plugins.treesitter',
+    require 'plugins.telescope',
+    require 'plugins.lsp',
+    require 'plugins.autocompletion',
+    require 'plugins.autoformatting',
+    require 'plugins.alpha',
+    require 'plugins.indent-blankline',
+    require 'plugins.gitsigns',
+    require 'plugins.misc',
+    require 'plugins.comment'
+})
